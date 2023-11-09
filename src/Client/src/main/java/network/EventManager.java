75bc0a79d0558c500a7b01f8012da00a5046dd3d
SilverFlin <toledorusso@outlook.com>
2023-11-09 12:47:56 -0700

    feat: create joinToWaitingRoom Event

--- src/Client/src/main/java/network/EventManager.java
+++ src/Client/src/main/java/network/EventManager.java
@@ -1,8 +1,15 @@
 package network;
 
+import client.GameSystemFacadeImpl;
+import domain.Avatar;
+import domain.DominoGame;
+import domain.Player;
 import edu.itson.eventschema.Event;
+import edu.itson.eventschema.PlayerJoinsEvent;
+import edu.itson.eventschema.UpdateGameEvent;
 import interfaces.GameSystemFacade;
 import ui.Board.GameDTO;
+import ui.MainMenu.AvatarDTO;
 import ui.MainMenu.PlayerDTO;
 
 /**
@@ -13,9 +20,22 @@ public class EventManager implements EventProducer, EventConsumer {
     private GameSystemFacade gameSystem;
     private NetworkConnection connection;
 
+    public EventManager(final NetworkEventConnection connection) {
+        this.connection = connection;
+        this.gameSystem = new GameSystemFacadeImpl();
+    }
+
     @Override
-    public void joinToWaitingRoom(PlayerDTO player) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void joinToWaitingRoom(final PlayerDTO playerDTO) {
+        Player player = new Player();
+
+        // TODO necesita casteo?
+        AvatarDTO avatarDTO = playerDTO.getAvatar();
+        Avatar avatar = new Avatar(avatarDTO.getNombre(), avatarDTO.getImage());
+        player.setAvatar(avatar);
+
+        Event updateWaitingRoomEvent = new PlayerJoinsEvent(player);
+        this.connection.sendMessage(updateWaitingRoomEvent);
     }
 
     @Override
@@ -24,8 +44,9 @@ public class EventManager implements EventProducer, EventConsumer {
     }
 
     @Override
-    public void consumeEvent(Event event) {
+    public void consumeEvent(final Event event) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
     }
 
 }
