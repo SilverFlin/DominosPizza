7177067bc2789e825b90b59acfc51108aeca0347
JIVB <darksamus0997@hotmail.com>
2023-11-09 21:59:32 -0700

    feat: PlayertileGraphic creado, le faltan implementaciones

---
+++ src/GraphicManager/src/main/java/com/mycompany/graphicmanager/PlayerTileGraphic.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.graphicmanager;
+
+import java.awt.geom.Point2D;
+
+/**
+ *
+ * @author JIVB
+ */
+public class PlayerTileGraphic extends GraphicComponent {
+    
+    //Tile tile;
+    Point2D point;
+    
+}
