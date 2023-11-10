fbd99a30bfe733aaf41cdc04a9ca6ca94be0de12
JIVB <darksamus0997@hotmail.com>
2023-11-09 22:00:53 -0700

    feat: PoolGraphic creado, le faltan implementaciones

---
+++ src/GraphicManager/src/main/java/com/mycompany/graphicmanager/PoolGraphic.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.graphicmanager;
+
+/**
+ *
+ * @author JIVB
+ */
+public class PoolGraphic extends GraphicComponent {
+    
+    int tileAmount;
+
+    public PoolGraphic(int tileAmount) {
+        this.tileAmount = tileAmount;
+    }
+
+    
+}
