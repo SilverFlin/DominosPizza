81f0ce722bb71ea01684cea15f86192bc6389808
SilverFlin <toledorusso@outlook.com>
2023-11-16 12:13:46 -0700

    feat: implement game presenter

--- src/Client/src/main/java/ui/game/GamePresenterImpl.java
+++ src/Client/src/main/java/ui/game/GamePresenterImpl.java
@@ -1,33 +1,54 @@
 package ui.game;
 
+import base.BasePresenter;
 import dtos.DominoDTO;
 import dtos.GameDTO;
 import dtos.PlayerDTO;
 import dtos.WaitingRoomDTO;
+import java.util.List;
 
 /**
  *
  */
-public class GamePresenterImpl implements GamePresenter {
+public class GamePresenterImpl extends BasePresenter implements GamePresenter {
+
+    private final GameView view;
+    private final GameModel model;
+
+    public GamePresenterImpl(final GameView view, final GameModel model) {
+        this.view = view;
+        this.model = model;
+    }
 
     @Override
-    public void putTileInBoard(DominoDTO tile) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void putTileInBoard(final DominoDTO tile) {
+        this.model.putTileInBoard(tile);
+        this.view.updateGame((GameViewModel) this.model);
     }
 
     @Override
     public void updateGame(GameDTO game) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.model.updateGame(game);
+        this.view.updateGame((GameViewModel) this.model);
+
     }
 
     @Override
     public void loadBoard(WaitingRoomDTO waitingRoom, PlayerDTO myPlayer) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        List<PlayerDTO> players = waitingRoom.getPlayers();
+
+        // TODO crear y repartir fichas (en modelo)
+        System.out.println("GamePresenterImpl.loadBoard() cargando listas vacias");
+        GameDTO gameDTO = new GameDTO(null, players, null, waitingRoom.getInitialTiles());
+
+        this.model.updateGame(gameDTO);
+        this.view.updateGame((GameViewModel) this.model);
     }
 
     @Override
     public void takeFromPool() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.model.getTileFromPool();
+        this.view.updateGame((GameViewModel) this.model);
     }
 
 }
