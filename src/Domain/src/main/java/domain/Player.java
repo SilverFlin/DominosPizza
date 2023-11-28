436e83e8e9a68e8be4ca8ed4c34273cf6f06bfd3
Antonio <victorglezb@gmail.com>
2023-11-28 12:24:23 -0700

    Añadido método SetNotReady
    Para setear que el usuario no está listo para comenzar el juego

--- src/Domain/src/main/java/domain/Player.java
+++ src/Domain/src/main/java/domain/Player.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 /**
  * Clase que representa a un jugador en el juego.
- *
  * Esta clase implementa la interfaz Serializable para permitir la serialización
  * de objetos Player.
  */
@@ -28,7 +27,6 @@ public class Player implements Serializable {
 
     /**
      * Obtiene la lista de fichas en la mano del jugador.
-     *
      * @return Lista de fichas en la mano del jugador.
      */
     public List<PlayerTile> getTilesInHand() {
@@ -37,7 +35,6 @@ public class Player implements Serializable {
 
     /**
      * Establece la lista de fichas en la mano del jugador.
-     *
      * @param tilesInHand Nueva lista de fichas en la mano del jugador.
      */
     public void setTilesInHand(final List<PlayerTile> tilesInHand) {
@@ -46,7 +43,6 @@ public class Player implements Serializable {
 
     /**
      * Obtiene el avatar asociado al jugador.
-     *
      * @return Avatar asociado al jugador.
      */
     public Avatar getAvatar() {
@@ -55,7 +51,6 @@ public class Player implements Serializable {
 
     /**
      * Establece el avatar asociado al jugador.
-     *
      * @param avatar Nuevo avatar asociado al jugador.
      */
     public void setAvatar(final Avatar avatar) {
@@ -64,7 +59,6 @@ public class Player implements Serializable {
 
     /**
      * Verifica si el jugador es un administrador.
-     *
      * @return Verdadero si el jugador es administrador, falso de lo contrario.
      */
     public boolean isAdmin() {
@@ -73,7 +67,6 @@ public class Player implements Serializable {
 
     /**
      * Establece si el jugador es administrador.
-     *
      * @param isAdmin Nuevo valor que indica si el jugador es administrador.
      */
     public void setIsAdmin(final boolean isAdmin) {
@@ -82,7 +75,6 @@ public class Player implements Serializable {
 
     /**
      * Agrega una ficha a la mano del jugador.
-     *
      * @param tile Ficha de dominó a agregar a la mano del jugador.
      */
     public void addTile(final DominoTile tile) {
@@ -91,7 +83,6 @@ public class Player implements Serializable {
 
     /**
      * Elimina una ficha de la mano del jugador.
-     *
      * @param tile Ficha de dominó a eliminar de la mano del jugador.
      */
     public void removeTile(final PlayerTile tile) {
@@ -100,7 +91,6 @@ public class Player implements Serializable {
 
     /**
      * Verifica si el jugador está listo para comenzar el juego.
-     *
      * @return Verdadero si el jugador está listo, falso de lo contrario.
      */
     public boolean isReady() {
@@ -112,5 +102,12 @@ public class Player implements Serializable {
      */
     public void setReady() {
         this.isReady = true;
+    }
+    
+    /**
+     * Establece que el jugador NO está listo para comenzar el juego.
+     */
+    public void setNotReady() {
+        this.isReady = false;
     }
 }
