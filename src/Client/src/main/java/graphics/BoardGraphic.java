313ee18390b3cda5c6acdde1bf829ef584022c2b
SilverFlin <toledorusso@outlook.com>
2023-12-03 17:57:35 -0700

    refactor: graphic board

--- src/Client/src/main/java/graphics/BoardGraphic.java
+++ src/Client/src/main/java/graphics/BoardGraphic.java
@@ -11,23 +11,23 @@ import java.awt.geom.Rectangle2D;
  */
 public class BoardGraphic extends GraphicComposite {
 
-    Rectangle2D.Double tablero;
+    Rectangle2D.Double boardRectangle;
 
-    double centro = 0;
-    double i;
-    double d;
+    double center = 0;
+    double startXCoord;
+    double endXCoord;
 
     public BoardGraphic() {
-        tablero = new Rectangle2D.Double();
+        this.boardRectangle = new Rectangle2D.Double();
     }
 
     @Override
     public void draw(Graphics2D g2) {
-        tablero.setFrameFromDiagonal(100, 100, rec.getWidth() - 100, rec.getHeight() - 100);
+        boardRectangle.setFrameFromDiagonal(100, 100, rec.getWidth() - 100, rec.getHeight() - 100);
         g2.setPaint(new Color(102, 102, 102));
         g2.fillRect(0, 0, (int) rec.getWidth(), (int) rec.getHeight());
         g2.setPaint(Color.white);
-        g2.fill(tablero);
+        g2.fill(boardRectangle);
 
         dibujaTren();
         for (GraphicComponent component : components) {
@@ -43,10 +43,9 @@ public class BoardGraphic extends GraphicComposite {
         Point point = new Point((int) (rec.getBounds().getCenterX() - 20), (int) rec.getBounds().getCenterY() - 40);
         BoardTileGraphic boardTileGraphic = ((BoardTileGraphic) this.components.get(0));
         boardTileGraphic.setP(point);
-        this.centro = boardTileGraphic.getP().getX();
-        this.i = this.centro;
-        this.d = this.centro - boardTileGraphic.rec.getWidth();
-
+        this.center = boardTileGraphic.getP().getX();
+        this.startXCoord = this.center;
+        this.endXCoord = this.center - boardTileGraphic.rec.getWidth();
     }
 
 }
