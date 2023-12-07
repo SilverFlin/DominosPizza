d4955806e8fe77fca23b1622db31df68966c4964
Edgar Emir Borbón Jimenez <edemboji@gmail.com>
2023-12-07 13:42:22 -0700

    correccion

--- src/Client/src/main/java/graphics/MyPlayerGraphic.java
+++ src/Client/src/main/java/graphics/MyPlayerGraphic.java
@@ -10,7 +10,7 @@ import java.awt.image.BufferedImage;
  * @author JIVB
  */
 public class MyPlayerGraphic extends GraphicComposite {
-    
+
     
 
     public MyPlayerGraphic() {
--- src/Client/src/main/java/graphics/Utils.java
+++ src/Client/src/main/java/graphics/Utils.java
@@ -1,7 +1,10 @@
 package graphics;
 
 import dtos.DominoDTO;
+import dtos.OpponentDTO;
+import dtos.PlayerDTO;
 import java.awt.Point;
+import java.awt.image.BufferedImage;
 import java.util.List;
 import ui.game.GameViewModel;
 
@@ -15,6 +18,18 @@ public class Utils {
     }
 
     public static DominoGameGraphic createDominoGameGraphic(final GameViewModel gameViewModel) {
+
+        PlayerDTO myPlayer = gameViewModel.getMyPlayer();
+        MyPlayerGraphic myPlyerGraphic = new MyPlayerGraphic();
+
+        AvatarGraphic myAvatarGraphic = new AvatarGraphic(myPlayer.getAvatar().getNombre(), new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB));
+        myPlyerGraphic.add(myAvatarGraphic);
+
+        PlayersGraphic oppnents = new PlayersGraphic();
+
+        for (OpponentDTO o : gameViewModel.getRoom()) {
+            oppnents.add(new AvatarGraphic(o.getAvatar().getNombre(), new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB)));
+        }
 
         List<DominoDTO> boardTiles = gameViewModel.getBoard();
         BoardGraphic boardGraphic = new BoardGraphic();
